e2f26f2c7d98121063a354bac1ce9bba937f4dd3
ahrorbek69 <axausa69@gmail.com>
2022-07-15 16:55:34 +0500

    kk

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css'
 
 import App from "./App";
 
+import { BrowserRouter as Router} from 'react-router-dom'
+
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('root')
 )
