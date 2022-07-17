ce85a5689f0ab661e9dbf8e4e2887aca8fd98037
ahrorbek69 <axausa69@gmail.com>
2022-07-17 19:31:55 +0500

    asdf

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const CreateContainer = () => {
   return (
     <div className='w-full h-auto'>
-      
+      <div className='w-[90%] md:w-[75%] border border-gray-200 rounded-lg'>
+        
+      </div>
     </div>
   )
 }
